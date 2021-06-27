95c0336340d600d968cf49a574d6eb3e52c7265a
fynn <f@fynn.it>
2021-06-27 14:31:14 +0300

    merged

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
-var moment = require('moment-timezone');
+const moment = require('moment-timezone');
 moment.tz.add("Europe/Berlin|CET CEST CEMT|-10 -20 -30|01010101010101210101210101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-2aFe0 11d0 1iO0 11A0 1o00 11A0 Qrc0 6i00 WM0 1fA0 1cM0 1cM0 1cM0 kL0 Nc0 m10 WM0 1ao0 1cp0 dX0 jz0 Dd0 1io0 17c0 1fA0 1a00 1ehA0 1a00 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|41e5");
+
+const markdownIt = require('markdown-it');
+const markdownItRenderer = new markdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+  qoutes: '»«',
+});
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.setTemplateFormats("html, liquid, md");
@@ -9,12 +17,16 @@ module.exports = function(eleventyConfig) {
       excerpt_separator: "---"
     });
 
-    eleventyConfig.addFilter("dateformat", function(dateIn) {
+    eleventyConfig.addFilter("dateformat", (dateIn) => {
       return moment(dateIn).tz('CET').format('DD.MMMM');
     });
+    eleventyConfig.addFilter("markdownify", (str) => {
+      return markdownItRenderer.renderInline(str);
+    });
 
-    eleventyConfig.addFilter("tlc", function(val) {
-      return val.toLowerCase();
+    eleventyConfig.addFilter("tlc", (val) => {
+      if(val) {
+      return val.toLowerCase();}
     });
 
     // eleventyConfig.addCollection("newsrev", function(collectionApi) {
